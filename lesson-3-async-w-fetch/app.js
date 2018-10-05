65522f4d717f226cdbb497f3b794629d67e3ab7f
Benjamin Simonson <bfsimon@gmail.com>
2018-10-05 16:00:10 -0400

    adds times articles

--- lesson-3-async-w-fetch/app.js
+++ lesson-3-async-w-fetch/app.js
@@ -13,9 +13,16 @@
             headers: {
                 Authorization: 'Client-ID e1437d6fd8949b9c4a7730100712ba3ab5189e8c4b0de8ea6cea36729d694b2e'
             }
-        }).then(response => response.json())
+        })
+        .then(response => response.json())
         .then(addImage)
         .catch(e => requestError(e, 'image'));
+
+        fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=b8d93960d275474b966139c1bed94f65`)
+        .then(response => response.json())
+        .then(addArticles)
+        .catch(e => requestError(e, 'articles'));
+
     });
 
     function addImage(data) {
@@ -32,6 +39,23 @@
         }
 
         responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles(data) {
+        let htmlContent = '';
+
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            const articles = data.response.docs;
+            htmlContent = '<ul>' + articles.map(article => `<li class="article">
+                <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                <p>${article.snippet}</p>
+            </li>`
+            ).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
     }
 
     function requestError(e, part) {
